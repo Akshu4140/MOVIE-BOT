a0e791f0b4dd35286a4b372df3941e442c96c28b
devilphantom <phantomdevilbot73@gmail.com>
2025-11-24 11:27:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,7 +35,7 @@ PICS = (environ.get('PICS', 'https://files.catbox.moe/xaugwv.jpg')).split()  # S
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://graph.org/file/56b5deb73f3b132e2bb73.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://files.catbox.moe/qbjpim.jpg'))
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', ''))
 FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg https://graph.org/file/56b5deb73f3b132e2bb73.jpg')).split()  # Fsub pic
 
 # ============================
