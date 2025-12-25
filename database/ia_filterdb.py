3b50706d1f06cf0739536f316cc2d4efb95c6db6
devilphantom <phantomdevilbot73@gmail.com>
2025-12-25 10:07:31 +0530

    Refactor Media classes by removing unnecessary fields
    Removed unused fields from Media and Media2 classes.

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -35,12 +35,8 @@ instance2 = Instance.from_db(db2)
 @instance.register
 class Media(Document):
     file_id = fields.StrField(attribute="_id")
-    file_ref = fields.StrField(allow_none=True)
     file_name = fields.StrField(required=True)
     file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=True)
-    mime_type = fields.StrField(allow_none=True)
-    caption = fields.StrField(allow_none=True)
 
     class Meta:
         indexes = ("$file_name",)
@@ -50,13 +46,9 @@ class Media(Document):
 @instance2.register
 class Media2(Document):
     file_id = fields.StrField(attribute="_id")
-    file_ref = fields.StrField(allow_none=True)
     file_name = fields.StrField(required=True)
     file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=True)
-    mime_type = fields.StrField(allow_none=True)
-    caption = fields.StrField(allow_none=True)
-
+    
     class Meta:
         indexes = ("$file_name",)
         collection_name = COLLECTION_NAME
@@ -112,12 +104,8 @@ async def save_file(media):
     try:
         record = saveMedia(
             file_id=file_id,
-            file_ref=file_ref,
             file_name=file_name,
             file_size=media.file_size,
-            file_type=media.file_type,
-            mime_type=media.mime_type,
-            caption=(media.caption.html if media.caption and INDEX_CAPTION else None),
         )
     except ValidationError as e:
         logger.exception(f"[VALIDATION ERROR] '{file_name}' → {e}")
