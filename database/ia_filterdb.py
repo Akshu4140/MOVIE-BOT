e1b4c6469514c69ebe2b846068502255964047c4
devilphantom <phantomdevilbot73@gmail.com>
2026-01-01 21:08:40 +0530

    Add file_ref and caption fields to Media classes

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -37,6 +37,8 @@ class Media(Document):
     file_id = fields.StrField(attribute="_id")
     file_name = fields.StrField(required=True)
     file_size = fields.IntField(required=True)
+    file_ref = fields.StrField(allow_none=True)
+    caption = fields.StrField(allow_none=False)
 
     class Meta:
         indexes = ("$file_name",)
@@ -48,6 +50,8 @@ class Media2(Document):
     file_id = fields.StrField(attribute="_id")
     file_name = fields.StrField(required=True)
     file_size = fields.IntField(required=True)
+    file_ref = fields.StrField(allow_none=True)
+    caption = fields.StrField(allow_none=False)
     
     class Meta:
         indexes = ("$file_name",)
