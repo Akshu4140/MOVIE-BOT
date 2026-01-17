7618ba37b2ba82d31b18794d82f8bb0b851fdfca
Akshu4140 <149081038+Akshu4140@users.noreply.github.com>
2026-01-17 20:22:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,7 +35,7 @@ PICS = (environ.get('PICS', 'https://files.catbox.moe/z1ox34.jpg')).split()  # S
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://graph.org/file/56b5deb73f3b132e2bb73.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://files.catbox.moe/kk1h5n.jpg'))
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://files.catbox.moe/k07uvy.jpg'))
 FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg https://graph.org/file/56b5deb73f3b132e2bb73.jpg')).split()  # Fsub pic
 
 # ============================
@@ -59,7 +59,7 @@ auth_channels     = environ.get("AUTH_CHANNELS", "-100")# Channels for force sub
 # ============================
 # Payment Configuration
 # ============================
-QR_CODE = environ.get('QR_CODE', 'https://files.catbox.moe/kk1h5n.jpg')    # QR code image for payments
+QR_CODE = environ.get('QR_CODE', 'https://files.catbox.moe/k07uvy.jpg')    # QR code image for payments
 OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'NOT AVALABLE')    # Owner UPI ID for payments
 
 STAR_PREMIUM_PLANS = {
